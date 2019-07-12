36d4cef94494cb52b7a1fefbd38b4964f3862d84
Codee King <codee.king@gmail.com>
2019-07-12 16:31:50 -0500

    Updates

---
+++ precache-manifest.9cb910c8f9c30e13e3ad69bfc43eec64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5841c520ac549ff72d7dae1fa16c958",
+    "url": "/Codeebk/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "64b430a5c7c867114f24",
+    "url": "/Codeebk/project-4-assessment.git/static/css/main.ac68ed13.chunk.css"
+  },
+  {
+    "revision": "d55962bf8ca803a34a65",
+    "url": "/Codeebk/project-4-assessment.git/static/js/2.a1ece0d8.chunk.js"
+  },
+  {
+    "revision": "64b430a5c7c867114f24",
+    "url": "/Codeebk/project-4-assessment.git/static/js/main.8e3a397a.chunk.js"
+  },
+  {
+    "revision": "53586112fa744f11e9fa",
+    "url": "/Codeebk/project-4-assessment.git/static/js/runtime~main.52aa8925.js"
+  }
+]);
